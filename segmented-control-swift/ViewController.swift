885adeaef49c7892710737de57014916d1756cdb
Bob Pascazio <bob@bytefly.com>
2015-04-14 09:39:22 -0400

    Initial Commit

---
+++ segmented-control-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  segmented-control-swift
+//
+//  Created by Bob Pascazio on 4/14/15.
+//  Copyright (c) 2015 ByteFly Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
